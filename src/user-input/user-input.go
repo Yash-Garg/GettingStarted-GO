e901a24cefe957aabf1e313554f46340592dc636
Yash Garg <ben10.yashgarg@gmail.com>
2018-11-21 17:35:43 +0530

    Update user-input.go

--- src/user-input/user-input.go
+++ src/user-input/user-input.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"bufio"
+    "fmt"
+    "bufio"
 )
 
 func main() {
 
     var age int
     fmt.Println("What is your age?")
-    _, err: fmt.Scan(&age)												    // Reading an integer
+    _, err: fmt.Scan(&age)							// Reading an integer
 
     reader := bufio.newReader(os.Stdin)
     var name string
     fmt.Println("What is your name?")
-    name, _ := reader.readString("\n")										// Reading a string
+    name, _ := reader.readString("\n")						// Reading a string
 
     fmt.Println("Your name is ", name, " and your age is ", age)
 }
