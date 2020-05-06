2df84413681931eb380de036fd69760453784c44
Yash Garg <ben10.yashgarg@gmail.com>
2020-05-06 18:20:46 +0530

    Update findian.go

--- findian/findian.go
+++ findian/findian.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	var str string
-	fmt.Print("\nEnter a string : ")
-	fmt.Scan(&str)
-	if (strings.HasPrefix(str, "i") || strings.HasPrefix(str, "I")) && (strings.HasSuffix(str, "n") || strings.HasSuffix(str, "N")) {
-		if strings.Contains(str, "a") || strings.Contains(str, "A") {
-			fmt.Print("\nFound!")
-		}
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Println("\nEnter a string : ")
+
+	reader.Scan()
+
+	input := strings.ToLower(reader.Text())
+
+	if (strings.HasPrefix(input, "i")) && (strings.HasSuffix(input, "n")) && (strings.Contains(input, "a")) {
+		fmt.Println("\nFound!")
 	} else {
-		fmt.Print("\nNot Found!")
+		fmt.Println("\nNot Found!")
 	}
 }
